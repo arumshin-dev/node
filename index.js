06e2ec388aaf4e8acfdcc36d96641187a0614827
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-03-28 11:01:44 +0000

    디스코드 봇 온라인 켜기

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+// Intents를 GatewayIntentBits를 사용하여 지정
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+const token = process.env.DISCORD_BOT_SECRET;
+
+client.on('ready', () => {
+  console.log("I'm in");
+  console.log(client.user.username);
+});
+
+client.on('messageCreate', (msg) => {
+  if (msg.author.id !== client.user.id) {
+    console.log("messageCreate");
+    console.log("msg.author.id",msg.author.id);
+    console.log("client.user.id",client.user.id);
+    console.log("msg", msg);
+    //msg.channel.send(msg.content.split('').reverse().join(''));
+    //msg.
+  }
+});
+
+client.login(token);
